aa09892c3bf13da6108e51fd38aa82dd8b509602
Ringzor09 <andreijipa1995@gmail.com>
2024-07-24 19:18:17 +0300

    Update trigger-workflow.js

--- api/trigger-workflow.js
+++ api/trigger-workflow.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const githubToken = process.env.GITHUB_TOKEN;
-    const repo = 'Ringzor09/adverlink';
-    const workflowId = 'playwright.yml';
+    const githubToken = process.env.GITHUB_TOKEN; // Ensure this is set in Vercel
+    const repo = 'Ringzor09/adverlink'; // Update with your GitHub repo
+    const workflowId = 'playwright.yml'; // Update with your GitHub Actions workflow filename
 
     try {
       await axios.post(`https://api.github.com/repos/${repo}/actions/workflows/${workflowId}/dispatches`, {
-        ref: 'main'
+        ref: 'main' // Branch name
       }, {
         headers: {
           'Authorization': `token ${githubToken}`,
